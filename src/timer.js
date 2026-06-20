635610c71718914b9fbee5ba662b99bc03308666
Theridel <simone.python3@gmail.com>
2026-06-20 10:24:51 +0000

    test ci-cd with docker

--- src/timer.js
+++ src/timer.js
@@ -1,1 +1,7 @@
 // Test intenzionale per far scattare ESLint concluso
+// Test di stress per Semgrep: questa funzione è volutamente insicura
+function testSecurityGate() {
+  const userInput = "<h1>Attacco XSS</h1>";
+  // Semgrep p/javascript odia l'uso diretto di innerHTML con variabili
+  document.getElementById('timer-display').innerHTML = userInput; 
+}
